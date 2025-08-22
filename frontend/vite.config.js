b118e9cf0b8a881d73655549c42f5740f2e205ae
amine <amineotako64@gmail.com>
2025-08-22 13:43:26 +0200

    Changes to be committed: 	modified:   frontend/vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,18 +2,30 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from "path";
 import tailwindcss from "@tailwindcss/vite"
-import { fileURLToPath } from 'url'; 
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  base:'/',
+  plugins: [react(), tailwindcss()],
+  base: '/',
   resolve: {
     alias: {
       // Now __dirname is correctly defined
       "@": path.resolve(__dirname, "./src")
     },
+  },
+  server:
+  {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+    host: '0.0.0.0',
+    port: 5173,
   },
 });
