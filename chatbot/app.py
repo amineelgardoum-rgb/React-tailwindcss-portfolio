42b16b8b509d990132837342bf8dab82d483c4fe
amine <amineotako64@gmail.com>
2025-08-25 13:49:11 +0200

    Changes to be committed: 	modified:   chatbot/app.py

--- chatbot/app.py
+++ chatbot/app.py
@@ -95,4 +95,4 @@ async def message(query:str):
         print("there is an error",e)
     
 if __name__=="__main__":
-    uvicorn.run("app:app",host="127.0.0.1",port=8000,reload=True)
+    uvicorn.run("app:app",host="0.0.0.0",port=8000,reload=True)
