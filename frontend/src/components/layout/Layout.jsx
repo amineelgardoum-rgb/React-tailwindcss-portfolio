b512cde1c8182304810b2fe338d78e227d835c9c
amineelgardoum-rgb <amine.elgardoum@etu.uae.ac.ma>
2026-05-29 12:12:06 +0100

    changed something

--- frontend/src/components/layout/Layout.jsx
+++ frontend/src/components/layout/Layout.jsx
@@ -6,18 +6,17 @@ import { MobileMenu } from './MobileMenu';
 import { useIsTouchDevice } from '../ui/MobileTouch'; 
 import { Background } from '../ui/Background';
 
-
 export const Layout = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   const isTouchDevice = useIsTouchDevice();
 
   return (
-    <div className="relative min-h-screen">
+    <div className="relative min-h-screen w-full">
       {!isTouchDevice && <CustomCursor />}
       <Background />
       <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-      <main className="relative z-10">
+      <main className="relative z-10 w-full pt-16">
         <Outlet />
       </main>
     </div>
